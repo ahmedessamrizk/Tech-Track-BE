b3171127de09459c0bd50eeb0bbb366b55ba9c5a
SalmaAhmed17 <105376336+SalmaAhmed17@users.noreply.github.com>
2024-01-30 01:55:14 +0200

    Update Accessory.js

--- models/Accessory.js
+++ models/Accessory.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-
+const slugify = require('slugify');
 const accessorySchema = new mongoose.Schema({
     type:{
         type: String
@@ -10,7 +10,7 @@ const accessorySchema = new mongoose.Schema({
      name: {
         type: String
      },
-     'model-name': {
+     model_name: {
         type: String
      },
      price: {
@@ -20,22 +20,22 @@ const accessorySchema = new mongoose.Schema({
         type: Number
      },
      
-     'image-src': {
+     image_src: {
         type: String
      },
      rate: {
         type: Number
      },
-     'no-rates': {
+     no_rates: {
         type: Number
      },
-     'link-href': {
+     link_href: {
         type: String
      },
      features: {
         type: String
      },
-     'rate-weight': {
+     rate_weight: {
         type: Number
      },
      site: {
@@ -49,5 +49,14 @@ const accessorySchema = new mongoose.Schema({
     { collection: 'Accessories' }
 );
 
-const Accessory = mongoose.model('Accessory', accessorySchema);
+//const Laptop = mongoose.model('Laptop', laptopSchema);
+accessorySchema.pre('save', function(next){//pre means this middleware fn will be performed before a certain event eli hwa el save hena
+   // console.log(this); //as this is a doc middleware so this here refers to the doc being processed at this point of time
+   this.slug = slugify(this.name,{lower: true} );
+   next(); //to call the next middleware in the stack
+});
+
+
+const Accessory = mongoose.model('Accessory', accessorySchema, 'Accessories');
 module.exports = Accessory;
+ 
